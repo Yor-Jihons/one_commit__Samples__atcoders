bf7ce3842882b309bac88211eb13ede107f52f49
Yor-Jihons <ryo.malon814@gmail.com>
2023-03-12 20:30:54 +0900

    2023/3/12

---
+++ A__box/src/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<sstream>
+#include<algorithm>
+#include<memory>
+#include<limits>
+
+using namespace std;
+
+int main( int argc, char** argv ){
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    cout << n - a + b << endl;
+return 0;
+}
