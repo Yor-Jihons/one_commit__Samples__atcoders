7586c3f1b79157ccc76fd1ef7080f457b5771d7f
Yor-Jihons <ryo.malon814@gmail.com>
2023-03-14 14:41:54 +0900

    Added the outline comments.

--- systems/testor/src/main.cpp
+++ systems/testor/src/main.cpp
@@ -9,6 +9,11 @@ using namespace std;
 
 
 int main( int argc, char** argv ){
-    cout << "testor" << endl;
+    // 1. Parses the command-line arguments, then gains the file-paths as Input and Output.
+    // 2. Reads the file as input.
+    // 3. Reads the file as output.
+    // 4. Compare the lines with (2) and (3).
+        // 4.1. If the lines are diference, quit.
+
 return 0;
 }
