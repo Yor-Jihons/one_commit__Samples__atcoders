9acf34d623b94384b89807d589e09378c53e0942
Yor-Jihons <ryo.malon814@gmail.com>
2023-03-16 09:58:47 +0900

    Implemented the function ReadAllFile.

--- systems/testor/src/main.cpp
+++ systems/testor/src/main.cpp
@@ -35,6 +35,5 @@ int main( int argc, char** argv ){
     std::vector<std::string> input = IO::ReadAllFile( inputFilePath );
 
     for( int i = 0; i < input.size(); i++ ) cout << input[i] << endl;
-
 return 0;
 }
