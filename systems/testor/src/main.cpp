2200f67b0965d15a08cb445354932cbd7197d9f5
Yor-Jihons <ryo.malon814@gmail.com>
2023-03-19 11:16:10 +0900

    不要なコードを削除した

--- systems/testor/src/main.cpp
+++ systems/testor/src/main.cpp
@@ -16,23 +16,6 @@ using namespace Testor;
 int main( int argc, char** argv ){
     try{
         // 1. Parses the command-line arguments, then gains the file path as input and output.
-        if( argc < 4 ){
-            cout << "[CMD]" << endl;
-            cout << "$ testor <CMD> <INPUTFILEPATH> <OUTPUTFILEPATH>" << endl;
-            cout << endl;
-            cout << "[ARGUEMNTS]" << endl;
-            cout << "<CMD>:" << endl;
-            cout << "    Pass the path of the executive file which you want to run. (Required)" << endl;
-            cout << "<INPUTFILE>:" << endl;
-            cout << "    Pass the file path as input. (Required)" << endl;
-            cout << "<OUTPUTFILE>:" << endl;
-            cout << "    Pass the file path as output. (Required)" << endl;
-            return -1;
-        }
-        std::string cmd            = argv[1];
-        std::string inputFilePath  = argv[2];
-        std::string outputFilePath = argv[3];
-
         auto cmdline = CommandLines::CmdLine::Create( argc, argv );
 
         cout << "CMD    : " << cmdline->Cmd() << endl;
@@ -41,10 +24,10 @@ int main( int argc, char** argv ){
 
         // 2. Reads the file as input.
         //auto input = IO::ReadAllFile( inputFilePath );
-        auto actuals = Processes::RunProcess( Processes::Process::CreateCmd( cmd, inputFilePath ) );
+        auto actuals = Processes::RunProcess( Processes::Process::CreateCmd( cmdline->Cmd(), cmdline->InputFilePath() ) );
 
         // 3. Reads the file as input.
-        auto expected = IO::ReadAllFile( outputFilePath );
+        auto expected = IO::ReadAllFile( cmdline->OutputFilePath() );
 
         // 4. Compare (2) and (3).
         if( actuals.size() != expected.size() ){
