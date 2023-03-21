0942f82668e1606cf4d1de5704debb1f9b83e47c
Yor-Jihons <ryo.malon814@gmail.com>
2023-03-21 19:53:48 +0900

    Added the comments.

--- systems/testor/src/Comparision/vectorcomparer.h
+++ systems/testor/src/Comparision/vectorcomparer.h
@@ -1,3 +1,8 @@
+/**
+* @file
+* @brief To compare the expected values and actual values.
+*/
+
 #ifndef COMPARISION_COMPARER_H
 #define COMPARISION_COMPARER_H
 
@@ -5,13 +10,28 @@
 #include<string>
 
 namespace Testor::Comparision{
+    /** @brief The class in order to compare the expected values and actual values. */
     class VectorComparer{
         public:
+            /**
+            * @brief Constructor.
+            * @param allow2ShowMessage_WhenError Whether error message will be shown when the error raised.
+            */
             VectorComparer( bool allow2ShowMessage_WhenError );
+
+            /**
+            * @brief Destructor.
+            */
             ~VectorComparer() = default;
 
+            /**
+            * @brief Constructor.
+            * @param expected The expected values.
+            * @param actuals The actual values.
+            */
             bool Compare( const std::vector<std::string>& expected, const std::vector<std::string>& actuals );
         private:
+            /// @brief Whether error message will be shown when the error raised.
             bool allow2ShowMessage_WhenError_;
     };
 }
