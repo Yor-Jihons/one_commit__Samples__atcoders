a345072fe88408b911636fd89fa1c5c1fbaea3be
Yor-Jihons <ryo.malon814@gmail.com>
2023-03-22 15:58:39 +0900

    Added the comments.

--- systems/testor/src/Exceptions/fileopenexception.h
+++ systems/testor/src/Exceptions/fileopenexception.h
@@ -1,12 +1,22 @@
-#ifndef EXCEPTIONS_FileOpenException_H
-#define EXCEPTIONS_FileOpenException_H
+/**
+* @file
+* @brief The exception which means opening file was failed.
+*/
+
+#ifndef EXCEPTIONS_FILEOPENEXCEPTION_H
+#define EXCEPTIONS_FILEOPENEXCEPTION_H
 
 #include<exception>
 #include<string>
 
 namespace Testor::Exceptions{
+    /// @brief The exception class which means opening file was failed.
     class FileOpenException : public std::runtime_error{
         public:
+            /**
+            * @brief Constructor.
+            * @param msg The message to show.
+            */
             FileOpenException( const std::string& msg ) : std::runtime_error(msg.c_str()){}
     };
 }
