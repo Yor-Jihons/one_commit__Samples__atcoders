53d1b089893fce4a462143e75bf39ea4fa2d07f0
Yor-Jihons <ryo.malon814@gmail.com>
2023-04-26 11:54:40 +0900

    Gets the command-line arguments.

--- systems/InstallTestor/InstallTestor.py
+++ systems/InstallTestor/InstallTestor.py
@@ -1,9 +1,12 @@
-print( "InstallTestor" )
-
+import sys
 
 # 0. Gets the command-line arguments.
+args = sys.argv
+if len( args ) <= 1:
+    print( "[CMD]\n$ Python InstallTestor.py <PROJECT_NAME> <NUMBER_OF_TESTCASE>" )
+    sys.exit( -1 )
+
 # 1. Reads the file as a CSV file.
-# 2. Loops for number of lines
+# 2. Loops for number of linesa
 #     2.1. Writes the file "testor" (as a makefile).
 #     2.2. Writes the file makefile (as a main makefile).
-
